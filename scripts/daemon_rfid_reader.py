bf3620065056a01d69df2789c4b7c4755f1afc07
Dominik Herold <dominik.herold@web.de>
2019-04-04 22:44:31 +0200

    add hack to use phoniebox like tonies where figure stay on top

--- scripts/daemon_rfid_reader.py
+++ scripts/daemon_rfid_reader.py
@@ -11,12 +11,15 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 
 print dir_path
 
+lastcardid = None
+
 while True:
         # reading the card id
         cardid = reader.reader.readCard()
         try:
             # start the player script and pass on the cardid
-            if cardid != None:
+            if cardid != None && cardid != lastcardid:
+                lastcardid = cardid
                 subprocess.call([dir_path + '/rfid_trigger_play.sh --cardid=' + cardid], shell=True)
         except OSError as e:
             print "Execution failed:" 
